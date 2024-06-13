c56600aa17f4061f38cf46d6a7dd77f430e1bdb2
Letlotlo Malope <letlotlomal@gmail.com>
2024-06-13 19:31:09 +0200

    Added State constant for fetched posts to be stored in and created asynchronous function to fetch posts from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React, from 'react';
+import React, {useEffect} from 'react';
 import "./App.css"
 
 function App (){
 
-  
-}
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      }
+  }
+  }
+
+)}

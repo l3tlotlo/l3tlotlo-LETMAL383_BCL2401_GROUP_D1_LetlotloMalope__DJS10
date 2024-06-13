ccef8471ff523a44740490a1b19290e7f663b327
Letlotlo Malope <letlotlomal@gmail.com>
2024-06-13 20:21:28 +0200

    added error handling state and Message to be thrown

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import "./App.css"
 function App (){
 
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
   useEffect(() => {
-
-    const fetchPosts = async () => {
+    const fetchPosts = async  () => {
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
       }
+       if (!response.ok) {
+          throw new Error('Posts not available'); 
+        }
   }
   }
 
 )} 
+ 
+export default App;

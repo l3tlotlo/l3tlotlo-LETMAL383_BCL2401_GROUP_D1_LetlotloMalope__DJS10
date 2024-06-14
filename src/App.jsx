de5c28de1dcd8555505d1e4e8103515d655c1d2f
Letlotlo Malope <letlotlomal@gmail.com>
2024-06-14 10:49:07 +0200

    parsed JSON data and updated posts state with data

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App (){
     const fetchPosts = async  () => {
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      }
-       if (!response.ok) {
+        if (!response.ok) {
           throw new Error('Posts not available'); 
         }
-
-  }
-  }
+        const data = await response.json();
+        setPosts(data);    
+        }
+      }
 
 )} 
 export default App;
